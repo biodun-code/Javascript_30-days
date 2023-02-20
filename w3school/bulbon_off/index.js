6401051168435e56dcb4c5869a6508bd29ad6991
majorshuga <biodunbiodun0000@gmail.com>
2023-02-20 10:21:45 +0100

    first commit

---
+++ w3school/bulbon_off/index.js
@@ -0,0 +1,7 @@
+document.getElementById("button").onclick = function () {
+  document.getElementById("myImage").src = "img/bulbon.gif";
+};
+
+document.getElementById("button2").onclick = function () {
+  document.getElementById("myImage").src = "img/pic_bulboff.gif";
+};
